72fe4b3c7c2103110a733ac5fed6708ce21eb6ba
Joel Rebello <jrebello@packet.com>
2022-11-16 14:54:44 +0100

    internal/worker: implements a worker loop to handler firmware install tasks.
    The worker identifies devices that require firmware installs
    and creates tasks for each device, queues them and executes the
    task to completion.

---
+++ internal/worker/worker_test.go
@@ -0,0 +1,45 @@
+package worker
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/metal-toolbox/flasher/internal/fixtures"
+	"github.com/metal-toolbox/flasher/internal/inventory"
+	"github.com/metal-toolbox/flasher/internal/model"
+	"github.com/metal-toolbox/flasher/internal/store"
+	"github.com/stretchr/testify/assert"
+)
+
+func initTestWorker() *Worker {
+	inv, _ := inventory.NewMockInventory()
+	return &Worker{
+		concurrency:  1,
+		taskMachines: sync.Map{},
+		store:        store.NewMemStore(),
+		inv:          inv,
+	}
+}
+
+func Test_CreateTaskForDevice(t *testing.T) {
+	worker := initTestWorker()
+
+	ctx := context.Background()
+
+	taskID, err := worker.enqueueTask(ctx, &inventory.DeviceInventory{Device: fixtures.Devices[fixtures.Device1.String()]})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, taskID)
+
+	tasks, err := worker.store.TasksByStatus(ctx, string(model.StateQueued))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 1, len(tasks))
+	assert.Equal(t, fixtures.Devices[fixtures.Device1.String()], tasks[0].Parameters.Device)
+	assert.Equal(t, model.PlanFromFirmwareSet, tasks[0].Parameters.FirmwarePlanMethod)
+}
