e38813aa0792bcf15f4acb2e3cb8f0c78443b0af
Joel Rebello <jrebello@packet.com>
2022-11-16 15:07:43 +0100

    internal/fixtures: add fixture data for tests

---
+++ internal/fixtures/device.go
@@ -0,0 +1,34 @@
+package fixtures
+
+import (
+	"net"
+
+	"github.com/metal-toolbox/flasher/internal/model"
+
+	"github.com/google/uuid"
+)
+
+var (
+	Device1 = uuid.New()
+	Device2 = uuid.New()
+
+	Devices = map[string]model.Device{
+		Device1.String(): {
+			ID:          Device1,
+			Vendor:      "dell",
+			Model:       "r6515",
+			BmcAddress:  net.ParseIP("127.0.0.1"),
+			BmcUsername: "root",
+			BmcPassword: "hunter2",
+		},
+
+		Device2.String(): {
+			ID:          Device2,
+			Vendor:      "dell",
+			Model:       "r6515",
+			BmcAddress:  net.ParseIP("127.0.0.2"),
+			BmcUsername: "root",
+			BmcPassword: "hunter2",
+		},
+	}
+)
