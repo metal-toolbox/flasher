f7fed7fa590bb9d8563dfeb96324be23453173e6
Joel Rebello <jrebello@packet.com>
2022-11-16 14:02:59 +0100

    internal/model: define consts, types and methods on the types

---
+++ internal/model/model.go
@@ -0,0 +1,122 @@
+package model
+
+import (
+	"context"
+	"net"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/bmc-toolbox/common"
+	"github.com/google/uuid"
+)
+
+type AppKind string
+
+const (
+	AppKindWorker AppKind = "worker"
+	AppKindClient AppKind = "client"
+
+	InventorySourceYaml          = "yaml"
+	InventorySourceServerservice = "serverservice"
+
+	LogLevelInfo  = 0
+	LogLevelDebug = 1
+	LogLevelTrace = 2
+)
+
+// AppKinds returns the supported flasher app kinds
+func AppKinds() []AppKind { return []AppKind{AppKindWorker, AppKindClient} }
+
+// InventorySourceKinds returns the supported asset inventory, firmware configuration sources
+func InventorySourceKinds() []string {
+	return []string{InventorySourceYaml, InventorySourceServerservice}
+}
+
+type Device struct {
+	ID uuid.UUID
+
+	// Device BMC attributes
+	BmcAddress  net.IP
+	BmcUsername string
+	BmcPassword string
+
+	// Inventory status attribute
+	State string
+
+	// Manufacturer attributes
+	Vendor string
+	Model  string
+	Serial string
+
+	// Device components
+	Components Components
+}
+
+// Firmware includes a firmware version attributes and is part of FirmwareConfig
+type Firmware struct {
+	Version       string `yaml:"version"`
+	URL           string `yaml:"URL"`
+	FileName      string `yaml:"filename"`
+	Utility       string `yaml:"utility"`
+	Model         string `yaml:"model"`
+	Vendor        string `yaml:"vendor"`
+	ComponentSlug string `yaml:"componentslug"`
+	Checksum      string `yaml:"checksum"`
+}
+
+// Firmwares is a list of firmwares with a SortByInstallOrder method.
+type Firmwares []Firmware
+
+// SortByInstallOrder sorts the firmwares planned in the order they should be installed
+func (p Firmwares) SortByInstallOrder() {
+	sort.Slice(p, func(i, j int) bool {
+		slugi := strings.ToLower(p[i].ComponentSlug)
+		slugj := strings.ToLower(p[j].ComponentSlug)
+		return FirmwareInstallOrder[slugi] < FirmwareInstallOrder[slugj]
+	})
+}
+
+var (
+	// FirmwareInstallOrder defines the order in which firmware is installed.
+	//
+	// TODO(joel): fix up bmc-toolbox/common slugs to be of lower case instead of upper
+	FirmwareInstallOrder = map[string]int{
+		strings.ToLower(common.SlugBMC):               0,
+		strings.ToLower(common.SlugBIOS):              1,
+		strings.ToLower(common.SlugCPLD):              2,
+		strings.ToLower(common.SlugDrive):             3,
+		strings.ToLower(common.SlugBackplaneExpander): 4,
+		strings.ToLower(common.SlugStorageController): 5,
+		strings.ToLower(common.SlugNIC):               6,
+		strings.ToLower(common.SlugPSU):               7,
+		strings.ToLower(common.SlugTPM):               8,
+		strings.ToLower(common.SlugGPU):               9,
+		strings.ToLower(common.SlugCPU):               10,
+	}
+)
+
+// DeviceQueryor interface defines methods to query a device.
+//
+// This is common interface to the ironlib and bmclib libraries.
+type DeviceQueryor interface {
+	// Open opens the connection to the device.
+	Open(ctx context.Context) error
+
+	// Close closes the connection to the device.
+	Close() error
+
+	PowerStatus(ctx context.Context) (status string, err error)
+
+	SetPowerState(ctx context.Context, state string) error
+
+	ResetBMC(ctx context.Context) error
+
+	// Inventory returns the device inventory
+	Inventory(ctx context.Context) (*common.Device, error)
+
+	// FirmwareInstall initiates the firmware install process returning a taskID for the install if any.
+	FirmwareInstall(ctx context.Context, componentSlug string, force bool, file *os.File) (taskID string, err error)
+
+	FirmwareInstallStatus(ctx context.Context, installVersion, componentSlug, bmcTaskID string) (ComponentFirmwareInstallStatus, error)
+}
---
+++ internal/model/task.go
@@ -0,0 +1,215 @@
+package model
+
+import (
+	"time"
+
+	sw "github.com/filanov/stateswitch"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+)
+
+// InstallMethod is one of 'outofband' OR 'inband'
+// it is the method by which the firmware is installed on the device.
+type InstallMethod string
+
+// FirmwarePlanMethod type defines the firmware resolution method by which
+// the firmware to applied is planned.
+type FirmwarePlanMethod string
+
+const (
+	// InstallMethodOutofband indicates the out of band firmware install method.
+	InstallMethodOutofband InstallMethod = "outofband"
+
+	// PlanPredefinedFirmaware is a TaskParameter attribute that indicates the
+	// firmware to be installed was provided at task initialization (through a CLI parameter or inventory device attribute)
+	// and so no further firmware planning is required.
+	PlanRequestedFirmware FirmwarePlanMethod = "predefined"
+
+	// PlanFromFirmwareSet is a TaskParameter attribute that indicates a
+	// firmware set ID was provided at task initialization (through a CLI parameter or inventory device attribute)
+	// the firmware versions to be installed are to be planned from the given firmware set ID.
+	PlanFromFirmwareSet FirmwarePlanMethod = "fromFirmwareSet"
+
+	// PlanFromInstalledFirmware is a TaskParameter attribute that indicates
+	// the firmware versions to be installed have to be planned
+	// based on the firmware currently installed on the device.
+	//PlanFromInstalledFirmware FirmwarePlanMethod = "fromInstalledFirmware"
+
+	// task states
+	//
+	// states the task transitions through
+	StateRequested sw.State = "requested"
+	StateQueued    sw.State = "queued"
+	StateActive    sw.State = "active"
+	StateSuccess   sw.State = "success"
+	StateFailed    sw.State = "failed"
+)
+
+var (
+	ErrTaskFirmwareParam = errors.New("error in task firmware parameters")
+)
+
+// Action is part of a task, it is resolved from the Firmware configuration
+//
+// Actions transition through states as the action progresses
+// those states are `queued`, `active`, `success`, `FailedState`.
+type Action struct {
+	// ID is a unique identifier for this action
+	ID string
+
+	// The parent task identifier
+	TaskID string
+
+	// BMCTaskID is the task identifier to track a BMC job
+	// these are returned whent he firmware install is initiated on the BMC.
+	BMCTaskID string
+
+	// Method of install
+	InstallMethod InstallMethod
+
+	// Status indicates the action status
+	Status string
+
+	// Firmware to be installed, this is set in the Task Plan phase.
+	Firmware Firmware
+
+	// FirwareTempFile is the temporary file downloaded to be installed.
+	//
+	// This is declared once the firmware file has been downloaded for install.
+	FirmwareTempFile string
+
+	// VerifyCurrentFirmware will cause the action to verify the current firmware
+	// on the component is not equal to one being installed. If its equal, the action will return an error.
+	VerifyCurrentFirmware bool
+
+	// BMCPowerCycleRequired is set when an action handler determines the BMC requires a reset.
+	BMCPowerCycleRequired bool
+
+	// HostPowerCycleRequired is set when an action handler determines the Host requires a reset.
+	HostPowerCycleRequired bool
+
+	// Final is set to true when its the last action being executed
+	Final bool
+}
+
+func (a *Action) SetState(state sw.State) error {
+
+	a.Status = string(state)
+
+	return nil
+}
+
+func (a *Action) State() sw.State {
+	return sw.State(a.Status)
+}
+
+// Actions is a list of actions
+type Actions []Action
+
+// ByID returns the Action matched by the identifier
+func (a Actions) ByID(id string) *Action {
+	for _, action := range a {
+		if action.ID == id {
+			return &action
+		}
+	}
+
+	return nil
+}
+
+// Task is a top level unit of work handled by flasher.
+//
+// A task performs one or more actions, each of the action corresponds to a Firmware planned for install.
+type Task struct {
+	// Task unique identifier
+	ID uuid.UUID
+
+	// Status is the install status
+	Status string
+
+	// Info is informational data and includes errors in task execution if any.
+	Info string
+
+	// ActionsPlanned to be executed for task are generated from the FirmwaresPlanned and install parameters
+	// these are generated in the `queued` stage of the task.
+	ActionsPlanned Actions
+
+	// FirmwaresPlanned is the list of firmware planned for install.
+	FirmwaresPlanned Firmwares
+
+	// Parameters for this task
+	Parameters TaskParameters
+
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	CompletedAt time.Time
+}
+
+// SetState implements the stateswitch statemachine interface
+func (t *Task) SetState(state sw.State) error {
+	t.Status = string(state)
+	return nil
+}
+
+// State implements the stateswitch statemachine interface
+func (t *Task) State() sw.State {
+	return sw.State(t.Status)
+}
+
+// NewTask returns a new Task
+//
+// method, device parameters are required.
+// firmwareSetID, firmware are optional and mutually exclusive.
+func NewTask(firmwareSetID string, firmware []Firmware) (Task, error) {
+	task := Task{ID: uuid.New()}
+
+	if firmwareSetID != "" && len(firmware) > 0 {
+		return task, errors.Wrap(ErrTaskFirmwareParam, "expected a firmware setID OR firmware version(s), got both")
+	}
+
+	if len(firmware) > 0 {
+		task.FirmwaresPlanned = firmware
+		task.Parameters.FirmwarePlanMethod = PlanRequestedFirmware
+
+		return task, nil
+	}
+
+	// firmwareSetID when defined sets the plan to PlanFromFirmwareSet,
+	// in the case where the firmwareSetID AND no firmware versions were defined
+	// the firmwareSetID is looked up in the task handlers.
+	if firmwareSetID != "" || (firmwareSetID == "" && len(firmware) == 0) {
+		task.Parameters.FirmwareSetID = firmwareSetID
+		task.Parameters.FirmwarePlanMethod = PlanFromFirmwareSet
+	}
+
+	return task, nil
+}
+
+// TaskParameters are the parameters set for each task flasher works
+// these are parameters received from an operator which determines the task execution actions.
+type TaskParameters struct {
+	// Reset device BMC before firmware install
+	ResetBMCBeforeInstall bool `json:"resetBMCBeforeInstall,omitempty"`
+
+	// Force install given firmware regardless of current firmware version.
+	ForceInstall bool `json:"forceInstall,omitempty"`
+
+	// Task priority is the task priority between 0 and 3
+	// where 0 is the default and 3 is the max.
+	//
+	// Tasks are picked from the `queued` state based on the priority.
+	//
+	// When there are multiple tasks with the same priority,
+	// the task CreatedAt attribute is considered.
+	Priority int `json:"priority,omitempty"`
+
+	// The firmware set ID is conditionally set at task initialization based on the FirmwareResolveMethod.
+	FirmwareSetID string `json:"firmwareSetID,omitempty"`
+
+	// Flasher determines the firmware to be installed for each component based on the firmware plan method,
+	// The method is set at task initialization
+	FirmwarePlanMethod FirmwarePlanMethod `json:"firmwarePlanMethod,omitempty"`
+
+	// Device attributes
+	Device Device `json:"-"`
+}
