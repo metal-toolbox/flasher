134c5192a2882a4215cfbfb84490bac46935f04e
Joel Rebello <jrebello@packet.com>
2022-11-16 14:07:12 +0100

    internal/store: store provides an interface to persist flasher task data
    This includes an implementation of a in-mem store.

---
+++ internal/store/interface.go
@@ -0,0 +1,17 @@
+package store
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/metal-toolbox/flasher/internal/model"
+)
+
+type Storage interface {
+	TasksByStatus(ctx context.Context, status string) ([]model.Task, error)
+	TaskByID(ctx context.Context, id string) (model.Task, error)
+	AddTask(ctx context.Context, task model.Task) (uuid.UUID, error)
+	UpdateTask(ctx context.Context, task model.Task) error
+	UpdateTaskAction(ctx context.Context, taskID string, action model.Action) error
+	RemoveTask(ctx context.Context, id string) error
+}
