5c9947fcbde649995a50ce0db5ec679f5ac70011
Doctor Vince <DoctorVin@users.noreply.github.com>
2023-06-22 12:40:17 -0400

    only set replicas when we have more than 1

--- internal/worker/liveness.go
+++ internal/worker/liveness.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"go.hollow.sh/toolbox/events"
+	"go.hollow.sh/toolbox/events/pkg/kv"
 	"go.hollow.sh/toolbox/events/registry"
 
 	"github.com/nats-io/nats.go"
@@ -14,6 +15,7 @@ import (
 var (
 	once           sync.Once
 	checkinCadence = 30 * time.Second
+	livenessTTL    = 3 * time.Minute
 )
 
 // This starts a go-routine to peridocally check in with the NATS kv
@@ -26,7 +28,16 @@ func (w *Worker) startWorkerLivenessCheckin(ctx context.Context) {
 			return
 		}
 
-		if err := registry.InitializeActiveControllerRegistry(natsJS); err != nil {
+		opts := []kv.Option{
+			kv.WithTTL(livenessTTL),
+		}
+
+		// any setting of replicas (even 1) chokes NATS in non-clustered mode
+		if w.replicaCount != 1 {
+			opts = append(opts, kv.WithReplicas(w.replicaCount))
+		}
+
+		if err := registry.InitializeRegistryWithOptions(natsJS, opts...); err != nil {
 			w.logger.WithError(err).Error("unable to initialize active worker registry")
 			return
 		}
