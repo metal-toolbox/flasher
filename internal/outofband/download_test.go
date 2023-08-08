97b9bfd81da9004186f4b2cffc728aff1999112f
Doctor Vince <DoctorVin@users.noreply.github.com>
2023-08-08 13:45:52 -0400

    add some tests

--- internal/outofband/download_test.go
+++ internal/outofband/download_test.go
@@ -8,24 +8,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_checksumValidate(t *testing.T) {
+func TestChecksumValidate(t *testing.T) {
 	tests := []struct {
 		testName      string
 		filename      string
 		checksum      string
-		expectedError string
+		expectedError error
 	}{
 		{
 			"no checksum prefix defined, default to md5",
 			"foo.bin",
 			"1649cff06611a6025da3dd511a97fb43", // file contents 'BLOB'
-			"",
+			nil,
 		},
 		{
 			"md5 prefix defined",
 			"foo.bin",
 			"md5sum:1649cff06611a6025da3dd511a97fb43", // file contents 'BLOB'
-			"",
+			nil,
+		},
+		{
+			"checksum is wrong",
+			"foo.bin",
+			"md5sum:bee8af7a84cb640cff90cf31fbf56950",
+			ErrChecksum,
+		},
+		{
+			"too many colons",
+			"foo.bin",
+			"md5sum:1649:cff06611a6025da3dd511a97fb43",
+			ErrFormat,
+		},
+		{
+			"unsupported digest format",
+			"foo.bin",
+			"vince:some-digest-format",
+			ErrFormat,
 		},
 	}
 	for _, tt := range tests {
@@ -40,8 +58,8 @@ func Test_checksumValidate(t *testing.T) {
 			defer os.Remove(binPath)
 
 			err = checksumValidate(binPath, tt.checksum)
-			if tt.expectedError != "" {
-				assert.Equal(t, tt.expectedError, err)
+			if tt.expectedError != nil {
+				assert.ErrorIs(t, err, tt.expectedError)
 				return
 			}
 
