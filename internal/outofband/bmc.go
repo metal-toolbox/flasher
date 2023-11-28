9e9ecbd7334d8edfa9a607a95f92c9124fa7447a
Joel Rebello <jrebello@packet.com>
2023-11-28 17:51:02 +0100

    outofband/bmc: pin bmclib provider and reinit bmclib client on disconnect
    - The bmc helper methods are updated to trace log the bmclib providers
      attempted for debugging.
    - The bmclib client is reinitialized if a session gets terminated or the
      connection was lost because the BMC reset.
    - Refactor to split out login retry method.

--- internal/outofband/bmc.go
+++ internal/outofband/bmc.go
@@ -3,6 +3,8 @@ package outofband
 import (
 	"context"
 	"os"
+	"path"
+	"runtime"
 	"strings"
 	"time"
 
@@ -40,12 +42,18 @@ var (
 	ErrTaskNotFound                = errors.New("task not found")
 	ErrFirmwareInstallFailed       = errors.New("firmware install failed")
 	ErrFirmwareTaskStateUnexpected = errors.New("BMC returned unexpected firmware task state")
+	ErrQueryorMethod               = errors.New("BMC queryor method error")
+
+	ErrFirmwareInstallProvider = errors.New("firmware install provider not identified")
 )
 
 // bmc wraps the bmclib client and implements the bmcQueryor interface
 type bmc struct {
-	client *bmclib.Client
-	logger *logrus.Entry
+	client             *bmclib.Client
+	logger             *logrus.Entry
+	asset              *model.Asset
+	installProvider    string
+	availableProviders []string
 }
 
 // NewDeviceQueryor returns a bmc queryor that implements the DeviceQueryor interface
@@ -53,6 +61,7 @@ func NewDeviceQueryor(ctx context.Context, asset *model.Asset, logger *logrus.En
 	return &bmc{
 		client: newBmclibv2Client(ctx, asset, logger),
 		logger: logger,
+		asset:  asset,
 	}
 }
 
@@ -64,14 +73,62 @@ func (e *ErrBmcQuery) Error() string {
 	return e.cause
 }
 
+func (b *bmc) tracelog() {
+	pc, _, _, _ := runtime.Caller(1)
+	funcName := path.Base(runtime.FuncForPC(pc).Name())
+
+	mapstr := func(m map[string]string) string {
+		if m == nil {
+			return ""
+		}
+
+		var s []string
+		for k, v := range m {
+			s = append(s, k+": "+v)
+		}
+
+		return strings.Join(s, ", ")
+	}
+
+	b.logger.WithFields(
+		logrus.Fields{
+			"attemptedProviders":   strings.Join(b.client.GetMetadata().ProvidersAttempted, ","),
+			"successfulProvider":   b.client.GetMetadata().SuccessfulProvider,
+			"successfulOpens":      strings.Join(b.client.GetMetadata().SuccessfulOpenConns, ","),
+			"successfulCloses":     strings.Join(b.client.GetMetadata().SuccessfulCloseConns, ","),
+			"failedProviderDetail": mapstr(b.client.GetMetadata().FailedProviderDetail),
+		}).Trace(funcName + ": connection metadata")
+}
+
+func (b *bmc) rebuildClient(ctx context.Context) {
+	newclient := newBmclibv2Client(ctx, b.asset, b.logger)
+	b.client = newclient
+}
+
 // Open creates a BMC session
 func (b *bmc) Open(ctx context.Context) error {
 	if b.client == nil {
 		return errors.Wrap(errBMCLogin, "bmclib client not initialized")
 	}
 
+	defer b.tracelog()
+
+	provider, err := b.provider()
+	if err != nil {
+		return errors.Wrap(ErrQueryorMethod, err.Error())
+	}
+
 	// login to the bmc with retries
-	return b.loginWithRetries(ctx, loginAttempts)
+	if err := b.loginWithRetries(ctx, loginAttempts, provider); err != nil {
+		return err
+	}
+
+	// save successful opens, since we want to make sure these are available for further actions.
+	if len(b.availableProviders) == 0 {
+		b.availableProviders = b.client.GetMetadata().SuccessfulOpenConns
+	}
+
+	return nil
 }
 
 // Close logs out of the BMC
@@ -83,13 +140,13 @@ func (b *bmc) Close(traceCtx context.Context) error {
 	ctxClose, cancel := context.WithTimeout(traceCtx, logoutTimeout)
 	defer cancel()
 
+	defer b.tracelog()
+
 	if err := b.client.Close(ctxClose); err != nil {
 		return errors.Wrap(errBMCLogout, err.Error())
 	}
 
 	b.logger.Debug("bmc logout successful")
-
-	b.client = nil
 
 	return nil
 }
@@ -100,7 +157,13 @@ func (b *bmc) PowerStatus(ctx context.Context) (string, error) {
 		return "", err
 	}
 
-	return b.client.GetPowerState(ctx)
+	provider, err := b.provider()
+	if err != nil {
+		return "", errors.Wrap(ErrQueryorMethod, "PowerStatus: "+err.Error())
+	}
+
+	defer b.tracelog()
+	return b.with(provider).GetPowerState(ctx)
 }
 
 // SetPowerState sets the given power state on the device
@@ -109,7 +172,13 @@ func (b *bmc) SetPowerState(ctx context.Context, state string) error {
 		return err
 	}
 
-	_, err := b.client.SetPowerState(ctx, state)
+	provider, err := b.provider()
+	if err != nil {
+		return errors.Wrap(ErrQueryorMethod, "SetPowerState: "+err.Error())
+	}
+
+	defer b.tracelog()
+	_, err = b.with(provider).SetPowerState(ctx, state)
 
 	return err
 }
@@ -120,8 +189,15 @@ func (b *bmc) ResetBMC(ctx context.Context) error {
 		return err
 	}
 
-	_, err := b.client.ResetBMC(ctx, "GracefulRestart")
+	provider, err := b.provider()
+	if err != nil {
+		return errors.Wrap(ErrQueryorMethod, "ResetBMC: "+err.Error())
+	}
 
+	defer b.tracelog()
+	_, err = b.with(provider).ResetBMC(ctx, "GracefulRestart")
+
+	defer b.tracelog()
 	return err
 }
 
@@ -131,7 +207,12 @@ func (b *bmc) Inventory(ctx context.Context) (*common.Device, error) {
 		return nil, err
 	}
 
-	inventory, err := b.client.Inventory(ctx)
+	provider, err := b.provider()
+	if err != nil {
+		return nil, errors.Wrap(ErrQueryorMethod, "Inventory: "+err.Error())
+	}
+
+	inventory, err := b.with(provider).Inventory(ctx)
 	if err != nil {
 		// increment inventory query error count metric
 		if strings.Contains(err.Error(), "no compatible System Odata IDs identified") {
@@ -144,57 +225,105 @@ func (b *bmc) Inventory(ctx context.Context) (*common.Device, error) {
 	// format the device inventory vendor attribute so its consistent
 	inventory.Vendor = common.FormatVendorName(inventory.Vendor)
 
+	defer b.tracelog()
 	return inventory, nil
 }
 
-func (b *bmc) FirmwareInstallSteps(ctx context.Context, component string) ([]bconsts.FirmwareInstallStep, error) {
-	if err := b.Open(ctx); err != nil {
+func (b *bmc) FirmwareInstallSteps(ctx context.Context, component string) (steps []bconsts.FirmwareInstallStep, err error) {
+	err = b.Open(ctx)
+	if err != nil {
 		return nil, err
 	}
 
-	return b.client.FirmwareInstallSteps(ctx, component)
+	defer b.tracelog()
+	steps, err = b.client.FirmwareInstallSteps(ctx, component)
+	if err != nil {
+		return nil, err
+	}
+
+	// pin the install provider once its identified
+	// this makes sure the subsequent firmware requests are performed using this provider.
+	b.installProvider = b.client.GetMetadata().SuccessfulProvider
+
+	// Validate we have a provider
+	//
+	// generally if the FirmwareInstallSteps method returned successfully this should not occur
+	if b.installProvider == "" {
+		return nil, ErrFirmwareInstallProvider
+	}
+
+	return steps, nil
 }
 
-func (b *bmc) FirmwareInstall(ctx context.Context, componentSlug string, force bool, file *os.File) (bmcTaskID string, err error) {
-	if err := b.Open(ctx); err != nil {
+func (b *bmc) FirmwareInstallUploadAndInitiate(ctx context.Context, component string, file *os.File) (taskID string, err error) {
+	err = b.Open(ctx)
+	if err != nil {
 		return "", err
+	}
+
+	provider, err := b.provider()
+	if err != nil {
+		return "", errors.Wrap(ErrQueryorMethod, "Inventory: "+err.Error())
 	}
 
 	installCtx, cancel := context.WithTimeout(ctx, firmwareInstallTimeout)
 	defer cancel()
 
-	return b.client.FirmwareInstall(installCtx, componentSlug, string(bconsts.OnReset), force, file)
+	defer b.tracelog()
+	return b.with(provider).FirmwareInstallUploadAndInitiate(installCtx, component, file)
 }
 
 // FirmwareTaskStatus looks up the firmware upload/install state and status values
-func (b *bmc) FirmwareTaskStatus(ctx context.Context, kind bconsts.FirmwareInstallStep, component, taskID, installVersion string, tryOpen bool) (state, status string, err error) {
+func (b *bmc) FirmwareTaskStatus(ctx context.Context, kind bconsts.FirmwareInstallStep, component, taskID, installVersion string, tryOpen bool) (state bconsts.TaskState, status string, err error) {
 	if tryOpen {
-		if err := b.Open(ctx); err != nil {
+		if err = b.Open(ctx); err != nil {
 			return "", "", errors.Wrap(ErrBMCQuery, err.Error())
 		}
 	}
 
-	return b.client.FirmwareTaskStatus(ctx, kind, component, taskID, installVersion)
+	provider, err := b.provider()
+	if err != nil {
+		return "", "", errors.Wrap(ErrQueryorMethod, "FirmwareTaskStatus: "+err.Error())
+	}
+
+	defer b.tracelog()
+	state, status, err = b.with(provider).FirmwareTaskStatus(ctx, kind, component, taskID, installVersion)
+
+	return state, status, err
 }
 
 func (b *bmc) FirmwareUpload(ctx context.Context, component string, file *os.File) (uploadTaskID string, err error) {
-	if err := b.Open(ctx); err != nil {
+	err = b.Open(ctx)
+	if err != nil {
 		return "", err
+	}
+
+	provider, err := b.provider()
+	if err != nil {
+		return "", errors.Wrap(ErrQueryorMethod, "FirmwareUpload: "+err.Error())
 	}
 
 	installCtx, cancel := context.WithTimeout(ctx, firmwareInstallTimeout)
 	defer cancel()
 
-	return b.client.FirmwareUpload(installCtx, component, file)
+	defer b.tracelog()
+	return b.with(provider).FirmwareUpload(installCtx, component, file)
 }
 
 func (b *bmc) FirmwareInstallUploaded(ctx context.Context, component, uploadVerifyTaskID string) (installTaskID string, err error) {
-	if err := b.Open(ctx); err != nil {
+	err = b.Open(ctx)
+	if err != nil {
 		return "", err
+	}
+
+	provider, err := b.provider()
+	if err != nil {
+		return "", errors.Wrap(ErrQueryorMethod, "FirmwareInstallUploaded: "+err.Error())
 	}
 
 	installCtx, cancel := context.WithTimeout(ctx, firmwareInstallTimeout)
 	defer cancel()
 
-	return b.client.FirmwareInstallUploaded(installCtx, component, uploadVerifyTaskID)
+	defer b.tracelog()
+	return b.with(provider).FirmwareInstallUploaded(installCtx, component, uploadVerifyTaskID)
 }
