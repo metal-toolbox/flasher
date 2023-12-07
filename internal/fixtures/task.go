6564c5261032593d93d97d44b61e74f53ca8fd9e
Joel Rebello <jrebello@packet.com>
2023-12-07 10:24:20 +0100

    go: update deps to latest, pin to bmclib v2.2.0

--- internal/fixtures/task.go
+++ internal/fixtures/task.go
@@ -4,7 +4,6 @@ import rctypes "github.com/metal-toolbox/rivets/condition"
 
 var (
 	TaskParametersA = rctypes.FirmwareInstallTaskParameters{
-		Priority:              0,
 		ResetBMCBeforeInstall: false,
 		ForceInstall:          false,
 	}
