cbe3251a1cbd764b696bfcacfe0019a5fcb0628b
Joel Rebello <jrebello@packet.com>
2024-04-16 10:49:38 +0200

    outofband/action_handlers: move to internal model.Step type
    Update tests to use testify/assert/mock

--- internal/outofband/action_handlers_test.go
+++ internal/outofband/action_handlers_test.go
@@ -7,242 +7,200 @@ import (
 
 	bconsts "github.com/bmc-toolbox/bmclib/v2/constants"
 	"github.com/bmc-toolbox/common"
-	sw "github.com/filanov/stateswitch"
-	"github.com/metal-toolbox/flasher/internal/fixtures"
+	"github.com/metal-toolbox/flasher/internal/device"
 	"github.com/metal-toolbox/flasher/internal/model"
-	sm "github.com/metal-toolbox/flasher/internal/statemachine"
+	"github.com/metal-toolbox/flasher/internal/runner"
+	rctypes "github.com/metal-toolbox/rivets/condition"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/mock/gomock"
 )
 
-type fakeStateSwitch struct{}
-
-func (_ fakeStateSwitch) State() sw.State {
-	return sw.State("bogus")
-}
-func (_ fakeStateSwitch) SetState(sw.State) error {
-	return nil
+func newTestActionCtx() *runner.ActionHandlerContext {
+	return &runner.ActionHandlerContext{
+		TaskHandlerContext: &runner.TaskHandlerContext{
+			Task: &model.Task{
+				Parameters: rctypes.FirmwareInstallTaskParameters{},
+				Asset:      &model.Asset{},
+				State:      model.StateActive,
+			},
+			Logger: logrus.NewEntry(logrus.New()),
+		},
+		Firmware: &model.Firmware{
+			Vendor:    "Dell-icious",
+			Version:   "DL6R",
+			URL:       "https://downloads.dell.com/FOLDER06303849M/1/Serial-ATA_Firmware_Y1P10_WN32_DL6R_A00.EXE",
+			FileName:  "Serial-ATA_Firmware_Y1P10_WN32_DL6R_A00.EXE",
+			Models:    []string{"r6515"},
+			Checksum:  "4189d3cb123a781d09a4f568bb686b23c6d8e6b82038eba8222b91c380a25281",
+			Component: "drive",
+		},
+	}
 }
 
 func TestCheckCurrentFirmware(t *testing.T) {
 	t.Parallel()
-	hPtr := &actionHandler{}
-	t.Run("type-check error", func(t *testing.T) {
-		t.Parallel()
-		ctx := &sm.HandlerContext{}
-		fss := &fakeStateSwitch{}
-		err := hPtr.checkCurrentFirmware(fss, ctx)
-		require.Error(t, err)
-		require.ErrorIs(t, err, sm.ErrActionTypeAssertion)
-	})
+
+	// helper func to initialize handler, mock device queryor
+	init := func(t *testing.T) (*handler, *device.MockQueryor) {
+		t.Helper()
+
+		actionCtx := newTestActionCtx()
+		m := new(device.MockQueryor)
+		m.On("FirmwareInstallSteps", mock.Anything, "drive").Once().Return(
+			[]bconsts.FirmwareInstallStep{
+				bconsts.FirmwareInstallStepUploadInitiateInstall,
+				bconsts.FirmwareInstallStepInstallStatus,
+			},
+			nil,
+		)
+		actionCtx.DeviceQueryor = m
+
+		ah := &ActionHandler{}
+		_, err := ah.ComposeAction(context.Background(), actionCtx)
+		assert.Nil(t, err)
+		ah.handler.action.FirmwareInstallStep = string(bconsts.FirmwareInstallStepUploadInitiateInstall)
+
+		return ah.handler, m
+	}
+
+	// helper func to debug device conversion
+	conversionDebug := func(t *testing.T, dev *common.Device) {
+		t.Helper()
+		exp, cErr := model.NewComponentConverter().CommonDeviceToComponents(dev)
+		require.NoError(t, cErr)
+		t.Logf("expected components length: %d\n", len(exp))
+		for i, c := range exp {
+			t.Logf("component %d => %#v\n", i, c)
+		}
+	}
+
+	ctx := context.Background()
+
 	t.Run("inventory error", func(t *testing.T) {
 		t.Parallel()
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		dq := fixtures.NewMockDeviceQueryor(ctrl)
-		ctx := &sm.HandlerContext{
-			Ctx:           context.Background(),
-			Logger:        logrus.NewEntry(&logrus.Logger{}),
-			DeviceQueryor: dq,
-		}
-		act := &model.Action{
-			VerifyCurrentFirmware: true,
-			Firmware: model.Firmware{
-				Component: "the-component",
-			},
-		}
-		dq.EXPECT().Inventory(gomock.Any()).Times(1).Return(nil, errors.New("pound sand"))
-		err := hPtr.checkCurrentFirmware(act, ctx)
+		handler, dq := init(t)
+
+		dq.EXPECT().Inventory(mock.Anything).Times(1).Return(nil, errors.New("pound sand"))
+		err := handler.checkCurrentFirmware(ctx)
 		require.Error(t, err)
 		require.Equal(t, "pound sand", err.Error())
 	})
+
 	t.Run("nil inventory", func(t *testing.T) {
 		t.Parallel()
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		dq := fixtures.NewMockDeviceQueryor(ctrl)
-		ctx := &sm.HandlerContext{
-			Ctx:           context.Background(),
-			Logger:        logrus.NewEntry(&logrus.Logger{}),
-			DeviceQueryor: dq,
-		}
-		act := &model.Action{
-			VerifyCurrentFirmware: true,
-			Firmware: model.Firmware{
-				Component: "the-component",
-			},
-		}
-		dq.EXPECT().Inventory(gomock.Any()).Times(1).Return(nil, nil)
-		err := hPtr.checkCurrentFirmware(act, ctx)
+		handler, dq := init(t)
+
+		dq.EXPECT().Inventory(mock.Anything).Times(1).Return(nil, nil)
+		err := handler.checkCurrentFirmware(ctx)
 		require.Error(t, err)
 		require.ErrorIs(t, err, model.ErrComponentConverter)
 	})
+
 	t.Run("bad component slug", func(t *testing.T) {
 		t.Parallel()
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		dq := fixtures.NewMockDeviceQueryor(ctrl)
-		ctx := &sm.HandlerContext{
-			Ctx:           context.Background(),
-			Logger:        logrus.NewEntry(&logrus.Logger{}),
-			DeviceQueryor: dq,
-		}
-		act := &model.Action{
-			VerifyCurrentFirmware: true,
-			Firmware: model.Firmware{
-				Component: "cool-bios",
-				Vendor:    "Dell-icious",
-				Models: []string{
-					"PowerEdge R6515",
-				},
-			},
-		}
+		handler, dq := init(t)
+
+		dev := common.NewDevice()
+		dev.Model = "PowerEdge R6515"
+		dev.Vendor = "Dell-icious"
+		dev.BIOS = &common.BIOS{}
+
+		dq.EXPECT().Inventory(mock.Anything).Times(1).Return(&dev, nil)
+		err := handler.checkCurrentFirmware(ctx)
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrComponentNotFound)
+	})
+
+	t.Run("blank installed version", func(t *testing.T) {
+		t.Parallel()
+		debug := false
+		handler, dq := init(t)
+
 		dev := common.NewDevice()
 		dev.Model = "PowerEdge R6515"
 		dev.Vendor = "Dell-icious"
 		dev.BIOS.Vendor = "Dell-icious"
-		dev.BIOS.Model = "Bios Model"
-		dq.EXPECT().Inventory(gomock.Any()).Times(1).Return(&dev, nil)
-		err := hPtr.checkCurrentFirmware(act, ctx)
-		require.Error(t, err)
-		require.ErrorIs(t, err, ErrComponentNotFound)
-	})
-	t.Run("blank installed version", func(t *testing.T) {
-		t.Parallel()
-		conversionDebug := false
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		dq := fixtures.NewMockDeviceQueryor(ctrl)
-		ctx := &sm.HandlerContext{
-			Ctx:           context.Background(),
-			Logger:        logrus.NewEntry(&logrus.Logger{}),
-			DeviceQueryor: dq,
-		}
-		act := &model.Action{
-			VerifyCurrentFirmware: true,
-			Firmware: model.Firmware{
-				Component: "bios",
-				Vendor:    "dell",
-				Models: []string{
-					"r6515",
+		dev.BIOS.Model = "r6515"
+		dev.BIOS.Serial = "12345"
+		dev.Drives = []*common.Drive{
+			{
+				Common: common.Common{
+					Vendor: "Dell-icious",
+					Model:  "r6515",
+					Firmware: &common.Firmware{
+						Installed: "", // no firmware version for component
+					},
 				},
 			},
 		}
-		dev := common.NewDevice()
-		dev.Model = "PowerEdge R6515"
-		dev.Vendor = "Dell-icious" // NB: This and the bogus BIOS vendor are ignored.
-		dev.BIOS.Vendor = "Dell-icious"
-		dev.BIOS.Model = "r6515"
-		dev.BIOS.Serial = "12345"
 
-		if conversionDebug {
-			exp, cErr := model.NewComponentConverter().CommonDeviceToComponents(&dev)
-			require.NoError(t, cErr)
-			t.Logf("expected components length: %d\n", len(exp))
-			for i, c := range exp {
-				t.Logf("component %d => %#v\n", i, c)
-			}
+		if debug {
+			conversionDebug(t, &dev)
 		}
 
-		dq.EXPECT().Inventory(gomock.Any()).Times(1).Return(&dev, nil)
-		err := hPtr.checkCurrentFirmware(act, ctx)
+		dq.EXPECT().Inventory(mock.Anything).Times(1).Return(&dev, nil)
+		err := handler.checkCurrentFirmware(ctx)
 		require.Error(t, err)
 		require.ErrorIs(t, err, ErrInstalledVersionUnknown)
 	})
 	t.Run("equal installed version", func(t *testing.T) {
 		t.Parallel()
-		conversionDebug := false
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		dq := fixtures.NewMockDeviceQueryor(ctrl)
-		ctx := &sm.HandlerContext{
-			Ctx:           context.Background(),
-			Logger:        logrus.NewEntry(&logrus.Logger{}),
-			DeviceQueryor: dq,
-		}
-		act := &model.Action{
-			TaskID:                "my-task-uuid",
-			VerifyCurrentFirmware: true,
-			Firmware: model.Firmware{
-				Component: "bios",
-				Vendor:    "dell",
-				Models: []string{
-					"r6515",
-				},
-				Version: "the version",
-			},
-		}
+		debug := false
+		handler, dq := init(t)
+
 		dev := common.NewDevice()
 		dev.Model = "PowerEdge R6515"
-		dev.Vendor = "Dell-icious" // NB: This and the bogus BIOS vendor are ignored.
-		dev.BIOS.Vendor = "Dell-icious"
-		dev.BIOS.Model = "r6515"
-		dev.BIOS.Serial = "12345"
-		dev.BIOS.Firmware = &common.Firmware{
-			Installed: "the version",
+		dev.Vendor = "Dell-icious"
+		dev.Drives = []*common.Drive{
+			{
+				Common: common.Common{
+					Vendor: "Dell-icious",
+					Model:  "r6515",
+					Firmware: &common.Firmware{
+						Installed: "DL6R", // match whats returned by init()
+					},
+				},
+			},
 		}
 
-		if conversionDebug {
-			exp, cErr := model.NewComponentConverter().CommonDeviceToComponents(&dev)
-			require.NoError(t, cErr)
-			t.Logf("expected components length: %d\n", len(exp))
-			for i, c := range exp {
-				t.Logf("component %d => %#v\n", i, c)
-			}
+		if debug {
+			conversionDebug(t, &dev)
 		}
 
-		dq.EXPECT().Inventory(gomock.Any()).Times(1).Return(&dev, nil)
-		err := hPtr.checkCurrentFirmware(act, ctx)
+		dq.EXPECT().Inventory(mock.Anything).Times(1).Return(&dev, nil)
+		err := handler.checkCurrentFirmware(ctx)
 		require.Error(t, err)
 		require.ErrorIs(t, err, ErrInstalledFirmwareEqual)
 	})
 	t.Run("installed version does not match", func(t *testing.T) {
 		t.Parallel()
-		conversionDebug := false
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		dq := fixtures.NewMockDeviceQueryor(ctrl)
-		ctx := &sm.HandlerContext{
-			Ctx:           context.Background(),
-			Logger:        logrus.NewEntry(&logrus.Logger{}),
-			DeviceQueryor: dq,
-		}
-		act := &model.Action{
-			TaskID:                "my-task-uuid",
-			VerifyCurrentFirmware: true,
-			Firmware: model.Firmware{
-				Component: "bios",
-				Vendor:    "dell", // this case has to match the vendor derived from the library
-				Models: []string{
-					"r6515",
-				},
-				Version: "the new version",
-			},
-		}
+		debug := false
+		handler, dq := init(t)
+
 		dev := common.NewDevice()
 		dev.Model = "PowerEdge R6515"
-		dev.Vendor = "Dell-icious" // NB: This and the bogus BIOS vendor are ignored.
-		dev.BIOS.Vendor = "Dell-icious"
-		dev.BIOS.Model = "r6515"
-		dev.BIOS.Serial = "12345"
-		dev.BIOS.Firmware = &common.Firmware{
-			Installed: "the version",
+		dev.Vendor = "Dell-icious"
+		dev.Drives = []*common.Drive{
+			{
+				Common: common.Common{
+					Vendor: "Dell-icious",
+					Model:  "r6515",
+					Firmware: &common.Firmware{
+						Installed: "OLDversion",
+					},
+				},
+			},
 		}
 
-		if conversionDebug {
-			exp, cErr := model.NewComponentConverter().CommonDeviceToComponents(&dev)
-			require.NoError(t, cErr)
-			t.Logf("expected components length: %d\n", len(exp))
-			for i, c := range exp {
-				t.Logf("component %d => %#v\n", i, c)
-			}
+		if debug {
+			conversionDebug(t, &dev)
 		}
 
-		dq.EXPECT().Inventory(gomock.Any()).Times(1).Return(&dev, nil)
-		err := hPtr.checkCurrentFirmware(act, ctx)
+		dq.EXPECT().Inventory(mock.Anything).Times(1).Return(&dev, nil)
+		err := handler.checkCurrentFirmware(ctx)
 		require.Nil(t, err)
 	})
 
@@ -281,57 +239,53 @@ func TestPollFirmwareInstallStatus(t *testing.T) {
 		},
 	}
 
+	init := func(t *testing.T) (*handler, *device.MockQueryor) {
+		t.Helper()
+
+		actionCtx := newTestActionCtx()
+		m := new(device.MockQueryor)
+		m.On("FirmwareInstallSteps", mock.Anything, "drive").Once().Return(
+			[]bconsts.FirmwareInstallStep{
+				bconsts.FirmwareInstallStepUploadInitiateInstall,
+				bconsts.FirmwareInstallStepInstallStatus,
+			},
+			nil,
+		)
+		actionCtx.DeviceQueryor = m
+
+		ah := &ActionHandler{}
+		_, err := ah.ComposeAction(context.Background(), actionCtx)
+		assert.Nil(t, err)
+		ah.handler.action.FirmwareInstallStep = string(bconsts.FirmwareInstallStepUploadInitiateInstall)
+
+		return ah.handler, m
+	}
+
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
-
-			task := newTaskFixture(string(model.StateActive))
-			asset := fixtures.Assets[fixtures.Asset1ID.String()]
-
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-			q := fixtures.NewMockDeviceQueryor(ctrl)
-
-			handlerCtx := &sm.HandlerContext{
-				Task:          task,
-				Ctx:           context.Background(),
-				Logger:        logrus.NewEntry(&logrus.Logger{}),
-				DeviceQueryor: q,
-				Asset:         &asset,
-			}
-
-			action := model.Action{
-				ID:       "foobar",
-				TaskID:   task.ID.String(),
-				Firmware: *fixtures.NewFirmware()[0],
-			}
-
-			_ = action.SetState(model.StateActive)
-			task.ActionsPlanned = append(task.ActionsPlanned, &action)
-
-			// init handler
-			handler := &actionHandler{}
+			handler, m := init(t)
 
 			os.Setenv(envTesting, "1")
 			defer os.Unsetenv(envTesting)
 
-			q.EXPECT().FirmwareTaskStatus(
-				gomock.Any(),
-				gomock.Any(),
-				gomock.Any(),
-				gomock.Any(),
-				gomock.Any(),
-			).AnyTimes().Return(bconsts.TaskState(tc.state), "some status", tc.errorContains)
+			m.EXPECT().FirmwareTaskStatus(
+				mock.Anything,
+				bconsts.FirmwareInstallStepUploadInitiateInstall,
+				"drive",
+				mock.Anything,
+				"DL6R",
+			).Return(bconsts.TaskState(tc.state), "some status", tc.errorContains)
 
 			if tc.state == "powercycle-host" {
-				q.EXPECT().SetPowerState(gomock.Any(), gomock.Any()).Times(1).Return(nil)
+				m.EXPECT().SetPowerState(mock.Anything, mock.Anything).Times(1).Return(nil)
 			}
 
-			if err := handler.pollFirmwareTaskStatus(&action, handlerCtx); err != nil {
+			if err := handler.pollFirmwareTaskStatus(context.Background()); err != nil {
 				if tc.errorContains != nil {
 					assert.ErrorContains(t, err, tc.errorContains.Error())
 				} else {
 					if tc.state == "powercycle-host" {
-						assert.True(t, action.HostPowerCycled)
+						assert.True(t, handler.action.HostPowerCycled)
 						return
 					}
 
